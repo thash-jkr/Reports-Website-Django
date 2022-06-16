d168c3c5606322b4241a4d8cc642a40775eae72a
thash-jkr <afthashsahal123@gmail.com>
2022-06-16 20:31:48 +0530

    Customer model

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    logo = models.ImageField(upload_to="customers", default="no_profile.png")
+
+    def __str__(self):
+        return self.name
