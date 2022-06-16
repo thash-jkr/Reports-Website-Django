a8116833d67706331bf956efcc25f10d29e7f256
thash-jkr <afthashsahal123@gmail.com>
2022-06-16 22:51:19 +0530

    Profile model and post_save signal

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,14 @@
+from venv import create
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default="No bio...")
+    avatar = models.ImageField(upload_to="profiles", default="no_profile.png")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
