ec4f4f08c7e9663e48480f18b99639171714a898
thash-jkr <afthashsahal123@gmail.com>
2022-06-16 22:14:26 +0530

    Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="products", default="no_product.png")
+    price = models.FloatField(help_text="in US dollars $")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.created.strftime('%d/%m/%Y')}"
