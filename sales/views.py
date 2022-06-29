d028e9401a2c6a521324f8dbc13e23002727a128
thash-jkr <afthashsahal123@gmail.com>
2022-06-29 20:02:07 +0530

    First form

--- sales/views.py
+++ sales/views.py
@@ -2,10 +2,23 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from .models import Sale
+from .forms import SalesSearchForm
 
 # Create your views here.
 def home(request):
-    context = {}
+    form = SalesSearchForm(request.POST or None)
+
+    if request.method == "POST":
+        date_from = request.POST.get("date_from")
+        date_to = request.POST.get("date_to")
+        chart_type = request.POST.get("chart_type")
+        print(date_from, date_to, chart_type, end=" :)")
+
+    hello = "Hello World!"
+    context = {
+        "hello": hello,
+        "form": form
+    }
     return render(request, "sales/home.html", context)
 
 
