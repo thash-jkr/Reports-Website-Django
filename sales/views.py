641bf9a8ae8a8687d83a4c7cb06cf63f480e864a
thash-jkr <afthashsahal123@gmail.com>
2022-07-01 19:29:28 +0530

    positions dataframe

--- sales/views.py
+++ sales/views.py
@@ -15,12 +15,24 @@ def home(request):
         date_from = request.POST.get("date_from")
         date_to = request.POST.get("date_to")
         chart_type = request.POST.get("chart_type")
-        qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+        sales_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
 
-        if len(qs) > 0:
-            sales_df = pd.DataFrame(qs.values())
+        if len(sales_qs) > 0:
+            sales_df = pd.DataFrame(sales_qs.values())
             sales_df = sales_df.to_html()
-            print(sales_df)
+            positions_data = []
+
+            for sale in sales_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        "position_id": pos.id,
+                        "product": pos.product.name,
+                        "quantity": pos.quantity,
+                        "price": pos.price
+                    }
+                    positions_data.append(obj)
+
+            positions_df = pd.DataFrame(positions_data).to_html()
         else:
             print("NO DATA")
 
@@ -28,7 +40,8 @@ def home(request):
     context = {
         "hello": hello,
         "form": form,
-        "sales_df": sales_df
+        "sales_df": sales_df,
+        "positions_df": positions_df
     }
     return render(request, "sales/home.html", context)
 
