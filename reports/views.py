680b480aca3e6bde31ed01b1131157c136211f7c
thash-jkr <afthashsahal123@gmail.com>
2022-07-04 20:53:43 +0530

    changed report saving method

--- reports/views.py
+++ reports/views.py
@@ -5,15 +5,24 @@ from numpy import imag
 from profiles.models import Profile
 from .models import Report
 from .utils import GetReportImage
+from .forms import ReportForm
 
 # Create your views here.
 def createReport(request):
+    form = ReportForm(request.POST or None)
     if request.method == "POST":
-        name = request.POST.get("name")
-        remarks = request.POST.get("remarks")
+        # name = request.POST.get("name")
+        # remarks = request.POST.get("remarks")
         image = request.POST.get("image")
         image = GetReportImage(image)
         author = Profile.objects.get(user=request.user)
-        Report.objects.create(name=name, remarks=remarks, image=image, author=author)
+        # Report.objects.create(name=name, remarks=remarks, image=image, author=author)
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.image = image
+            instance.author = author
+            instance.save()
+
         return JsonResponse({"msg": "send"})
     return JsonResponse({})
