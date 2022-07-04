b8e39d67a6acafd495017ebf203a6715884a1926
thash-jkr <afthashsahal123@gmail.com>
2022-07-04 21:15:48 +0530

    Added alerts to modal

--- sales/static/sales/base.js
+++ sales/static/sales/base.js
@@ -4,15 +4,19 @@ const reportBtn = document.getElementById("report-btn")
 const img = document.getElementById("img")
 const modalBody = document.getElementById("modal-body")
 const reportForm = document.getElementById("report-form")
+const alertBox = document.getElementById("alert-box")
 
 const reportName = document.getElementById("id_name")
 const reportRemarks = document.getElementById("id_remarks")
 const csrf = document.getElementsByName("csrfmiddlewaretoken")[0].value
-// console.log(reportName)
-// console.log(reportRemarks)
 
-// console.log(reportBtn)
-// console.log(img)
+const handleAlerts = (type, msg) => {
+    alertBox.innerHTML = `
+        <div class="alert alert-${type}" role="alert">
+            ${msg}
+        </div>
+    `
+}
 
 if (img) {
     reportBtn.classList.remove("not-visible")
@@ -21,8 +25,6 @@ if (img) {
 reportBtn.addEventListener("click", ()=>{
     img.setAttribute("class", "w-100")
     modalBody.prepend(img)
-
-    console.log(img.src)
 
     reportForm.addEventListener("submit", event=>{
         event.preventDefault()
@@ -38,9 +40,11 @@ reportBtn.addEventListener("click", ()=>{
             data: formData,
             success: function(response) {
                 console.log(response)
+                handleAlerts("success", "Report created!!")
             },
             error: function(response) {
                 console.log(response)
+                handleAlerts("danger", "Error; Not created!!")
             },
             processData: false,
             contentType: false
